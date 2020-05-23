f5471dc2f83d371e09f9b491e79ce1c68f6cd66f
bordobereli27 <65816082+bordobereli27@users.noreply.github.com>
2020-05-24 00:41:54 +0300

    smsddos

---
+++ yusuf.sh
@@ -0,0 +1,9 @@
+echo "<font color=aqua>Merhaba Ben Şimdi Bir Kaç Script İndirmem Gerekiyor Lütfen Bekleyin İndirdigimde Haber Edecegim Size</font>"
+pkg install python
+pkg install python3
+pkg install python3-pip
+pip install requests
+pip install colorama
+echo "<font color=aqua>Dosyaları yükledim Çalıştırıp ( Hedef No ) Girip Saldırıyı Başlatabilirsiniz</font>"
+python3 bomber.py
+
